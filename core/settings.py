099d5f430c650b3d0240d3474372eea2e5f7a1d0
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-05-23 20:39:13 +0200

    Register common app in project settings

--- core/settings.py
+++ core/settings.py
@@ -23,111 +23,112 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'watcher',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "watcher",
+    "common",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "default-secret-key")
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'webwatchdb'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('POSTGRES_HOST', 'db'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "webwatchdb"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = "UTC"
 
 CELERY_BEAT_SCHEDULE = {
-    'check_5min_tasks': {
-        'task': 'watcher.tasks.check_scheduled_tasks',
-        'schedule': 10.0,
-        'args': ('5min',),
+    "check_5min_tasks": {
+        "task": "watcher.tasks.check_scheduled_tasks",
+        "schedule": 10.0,
+        "args": ("5min",),
     },
-    'check_15min_tasks': {
-        'task': 'watcher.tasks.check_scheduled_tasks',
-        'schedule': crontab(minute='*/15'),
-        'args': ('15min',),
+    "check_15min_tasks": {
+        "task": "watcher.tasks.check_scheduled_tasks",
+        "schedule": crontab(minute="*/15"),
+        "args": ("15min",),
     },
-    'check_30min_tasks': {
-        'task': 'watcher.tasks.check_scheduled_tasks',
-        'schedule': crontab(minute='*/30'),
-        'args': ('30min',),
+    "check_30min_tasks": {
+        "task": "watcher.tasks.check_scheduled_tasks",
+        "schedule": crontab(minute="*/30"),
+        "args": ("30min",),
     },
-    'check_1hour_tasks': {
-        'task': 'watcher.tasks.check_scheduled_tasks',
-        'schedule': crontab(minute=0, hour='*/1'),
-        'args': ('1h',),
+    "check_1hour_tasks": {
+        "task": "watcher.tasks.check_scheduled_tasks",
+        "schedule": crontab(minute=0, hour="*/1"),
+        "args": ("1h",),
     },
-    'check_2hour_tasks': {
-        'task': 'watcher.tasks.check_scheduled_tasks',
-        'schedule': crontab(minute=0, hour='*/2'),
-        'args': ('2h',),
+    "check_2hour_tasks": {
+        "task": "watcher.tasks.check_scheduled_tasks",
+        "schedule": crontab(minute=0, hour="*/2"),
+        "args": ("2h",),
     },
-    'check_4hour_tasks': {
-        'task': 'watcher.tasks.check_scheduled_tasks',
-        'schedule': crontab(minute=0, hour='*/4'),
-        'args': ('4h',),
+    "check_4hour_tasks": {
+        "task": "watcher.tasks.check_scheduled_tasks",
+        "schedule": crontab(minute=0, hour="*/4"),
+        "args": ("4h",),
     },
-    'check_8hour_tasks': {
-        'task': 'watcher.tasks.check_scheduled_tasks',
-        'schedule': crontab(minute=0, hour='*/8'),
-        'args': ('8h',),
+    "check_8hour_tasks": {
+        "task": "watcher.tasks.check_scheduled_tasks",
+        "schedule": crontab(minute=0, hour="*/8"),
+        "args": ("8h",),
     },
-    'check_24hour_tasks': {
-        'task': 'watcher.tasks.check_scheduled_tasks',
-        'schedule': crontab(minute=0, hour=0),
-        'args': ('24h',),
+    "check_24hour_tasks": {
+        "task": "watcher.tasks.check_scheduled_tasks",
+        "schedule": crontab(minute=0, hour=0),
+        "args": ("24h",),
     },
 }
 
@@ -137,16 +138,16 @@ CELERY_BEAT_SCHEDULE = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -154,9 +155,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -166,54 +167,54 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {message}",
+            "style": "{",
         },
-        'simple': {
-            'format': '{levelname} {message}',
-            'style': '{',
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'info.log',
-            'maxBytes': 1024*1024*5,  # 5 MB
-            'backupCount': 5,  # keep at most 5 log files
-            'formatter': 'verbose',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': True,
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "info.log",
+            "maxBytes": 1024 * 1024 * 5,  # 5 MB
+            "backupCount": 5,  # keep at most 5 log files
+            "formatter": "verbose",
         },
-        'django.info': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+        "django.info": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": False,
         },
     },
 }
