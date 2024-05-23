196192b03bd9b1259c3d3ee78a1ca69df023c247
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-05-23 15:24:10 +0200

    Add urls from watcher to global project urls

--- core/urls.py
+++ core/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from watcher.views import create_task, strategy_form
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tasks/create/', create_task, name='create-task'),
-    path('tasks/<str:strategy_name>/create/', strategy_form, name='strategy-form')
+    path("admin/", admin.site.urls),
+    path("watch-task/", include("watcher.urls")),
 ]
