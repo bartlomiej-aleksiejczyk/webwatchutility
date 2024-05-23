0f1a2bdf9eebd12884d5a160b696584c1356acac
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-05-23 21:28:01 +0200

    Reformat wait for db script

--- wait_for_db.py
+++ wait_for_db.py
@@ -1,18 +1,19 @@
 import time
-import psycopg2
 from django.db import connections
 from django.db.utils import OperationalError
+
 
 def wait_for_db():
     db_conn = None
     while not db_conn:
         try:
-            db_conn = connections['default']
+            db_conn = connections["default"]
             c = db_conn.cursor()
         except OperationalError:
-            print('Database unavailable, waiting 1 second...')
+            print("Database unavailable, waiting 1 second...")
             time.sleep(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     wait_for_db()
-    print('Database available!')
+    print("Database available!")
