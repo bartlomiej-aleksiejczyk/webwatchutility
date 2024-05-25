86c40f5ede946b0d5b13af6da7b52b9aa7bf7c32
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-05-25 15:50:08 +0200

    Recreate migrations due to user model change

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("watch-task/", include("watcher.urls")),
     path("users/", include("users.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("common.urls")),
 ]
--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-from django.contrib.auth import views as auth_views
-
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("role",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("role",)}),)
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
