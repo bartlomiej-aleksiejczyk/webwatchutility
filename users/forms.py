f9e2903ad56d03e4313fb1bf08e573a74e4c488b
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-05-25 23:55:15 +0200

    Alter registration form to register non activated user

--- users/forms.py
+++ users/forms.py
@@ -5,3 +5,10 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_active = False
+        if commit:
+            user.save()
+        return user
